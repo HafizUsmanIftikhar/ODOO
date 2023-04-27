9d8aaf114c1193c4941b7be56529b0a82c1bbae7
HafizUsmanIftikhar <102325194+HafizUsmanIftikhar@users.noreply.github.com>
2023-04-27 22:09:44 +0500

    Add files via upload

---
+++ cms_base/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'University',
+    'version': '16.0.1.0.0',
+    'sequence':-100,
+    'author': 'BSCS',
+    'website': 'http://www.edu.pk',
+    'category': 'University Management System',
+    'license': "AGPL-3",
+    'Summary': 'A Module For university Management System',
+    'images': ['static/description/icon.jpg'],
+    'depends': [],
+    'data': ['views/cms_base_manu.xml',
+             #'views/cms_base.xml'
+             ],
+            
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    #'post_init_hook': '_method_run_before_installation',
+}
